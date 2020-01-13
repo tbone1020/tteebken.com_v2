16ea37bb203414bbf4ee8cc10f95da1b5bc641f0
Trent Teebken <trenttee1020@gmail.com>
2020-01-12 19:38:09 -0800

    Added portfolio service, Mock Portfolio service, and Portfolio JSON object

---
+++ src/app/shared/models/portfolio-item.ts
@@ -0,0 +1,12 @@
+export class PortfolioItem {
+
+    constructor(
+        public name: string, 
+        public imagePath: string, 
+        public description: string,
+        public liveDemo: string,
+        public github: string,
+        public tachnologies: string[]) {
+
+    }
+}
